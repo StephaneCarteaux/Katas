8802934399e98cb6382f31679aac8fb81de5b522
Stéphane Carteaux <stephanecarteaux@gmail.com>
2024-12-25 08:48:39 +0100

    Update numerology.php

--- kata-numerology/numerology.php
+++ kata-numerology/numerology.php
@@ -3,42 +3,22 @@ $n = readline("Introduce un numero: ");
 
 while ($n > 9) {
     $array = str_split($n);
-    var_dump($array);
     $n = array_sum($array);
-    var_dump($n);
 }
 
 echo $n . " ";
 
-switch ($n) {
-    case 0:
-        echo "el tot." . PHP_EOL;
-        break;
-    case 1:
-        echo "individualitat." . PHP_EOL;
-        break;
-    case 2:
-        echo "dualitat." . PHP_EOL;
-        break;
-    case 3:
-        echo "raó." . PHP_EOL;
-        break;
-    case 4:
-        echo "estabilitat." . PHP_EOL;
-        break;
-    case 5:
-        echo "amor." . PHP_EOL;
-        break;
-    case 6:
-        echo "destrucció." . PHP_EOL;
-        break;
-    case 7:
-        echo "saviessa." . PHP_EOL;
-        break;
-    case 8:
-        echo "poder." . PHP_EOL;
-        break;
-    case 9:
-        echo "generositat." . PHP_EOL;
-        break;
-}
+$result = match ($n) {
+    0 => "el tot.",
+    1 => "individualitat.",
+    2 => "dualitat.",
+    3 => "raó.",
+    4 => "estabilitat.",
+    5 => "amor.",
+    6 => "destrucció.",
+    7 => "saviessa.",
+    8 => "poder.",
+    9 => "generositat.",
+};
+
+echo $result;
